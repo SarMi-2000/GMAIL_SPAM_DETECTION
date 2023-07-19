3634834692c9972e86ef6a006d390271500495b5
Sarmistha Bera <68506912+SarMi-2000@users.noreply.github.com>
2023-07-19 13:10:48 +0530

    Update spam.py

--- spam.py
+++ spam.py
@@ -1,36 +1,64 @@
-
 import numpy as np
 import pandas as pd
-data=pd.read_csv('Spam_doc.csv',encoding = "ISO-8859-1")
-data['spam']=data['type'].map({'spam':1,'ham':0}).astype(int)
+
+# Read the data from the CSV file
+data=pd.read_csv('SPAM.csv',encoding = "ISO-8859-1")
+
+# Map 'spam' and 'ham' labels to binary values
+data['spam'] = data['type'].map({'spam': 1, 'ham': 0}).astype(int)
+
+# Tokenize the text
 def tokenizer(text):
     return text.split()
-data['text']=data['text'].apply(tokenizer)
+
+data['text'] = data['text'].apply(tokenizer)
+
+# Perform stemming using SnowballStemmer
 from nltk.stem.snowball import SnowballStemmer
-port_it=SnowballStemmer("english",ignore_stopwards=False)
+port_it = SnowballStemmer("english", ignore_stopwords=False)
+
 def stem_it(text):
     return [port_it.stem(word) for word in text]
-data['text']=data['text'].apply(stem_it)    
+
+data['text'] = data['text'].apply(stem_it)
+
+# Perform lemmatization using WordNetLemmatizer
 from nltk.stem import WordNetLemmatizer
 lemmatizer = WordNetLemmatizer()
-def lemmit_it(text):
-    return [lemmatizer.lemmatize(word,pos="a") for word in text]
-data['text']=data['text'].apply(lemmit_it)
+
+def lemmatize_it(text):
+    return [lemmatizer.lemmatize(word, pos="a") for word in text]
+
+data['text'] = data['text'].apply(lemmatize_it)
+
+# Remove stopwords using NLTK's stopwords corpus
 from nltk.corpus import stopwords
-stop_words=stopwords.words("english")
-def stop_it(text):
-    review=[word for word in text if not word in stop_words]
-data['text']=data['text'].apply(stop_it)    
-from sklearn.feature_extraction.text import TfidVectorizer
-tfidf=TfidVectorizer()
-y=data.spam.values
-x=tfidf.fit_transform(data['text'])
+stop_words = stopwords.words("english")
+
+def remove_stopwords(text):
+    return [word for word in text if not word in stop_words]
+
+data['text'] = data['text'].apply(remove_stopwords)
+
+# Perform TF-IDF vectorization
+from sklearn.feature_extraction.text import TfidfVectorizer
+tfidf = TfidfVectorizer()
+y = data.spam.values
+x = tfidf.fit_transform(' '.join(text) for text in data['text'])
+
+# Split the data into training and testing sets
 from sklearn.model_selection import train_test_split
-x_train,x_text,y_train,y_text=train_test_split(x,y,random_state=1,test_size=0.2,shuffle=False)
+x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=1, test_size=0.2, shuffle=False)
+
+# Train a logistic regression classifier
 from sklearn.linear_model import LogisticRegression
-clf=LogisticRegression()
-clf.fit(x_train,y_train)
-y_pred=clf.predict(x_text)
+clf = LogisticRegression()
+clf.fit(x_train, y_train)
+
+# Make predictions on the test set
+y_pred = clf.predict(x_test)
+
+# Calculate the accuracy of the classifier
 from sklearn.metrics import accuracy_score
-acc_linear_svc=accuracy_score(y_pred,y_text)*100
-print("accuracy:",acc_linear_svc)
+accuracy = accuracy_score(y_pred, y_test) * 100
+print("Accuracy:", accuracy)
